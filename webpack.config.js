62d0700ab620c12b10dbf211311d1b0c8a764793
Jim Yang <jim@prosewell.com>
2018-10-14 15:47:41 -0400

    v1 of first slide

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ module.exports = {
     new CopyWebpackPlugin([
       { from: path.join(__dirname, 'src/index.html'), to: path.join(__dirname, 'dist/index.html') },
       { from: path.join(__dirname, 'src/index.css'), to: path.join(__dirname, 'dist/index.css') },
+      { from: path.join(__dirname, 'src/images/'), to: path.join(__dirname, 'dist/images/') },
     ])
   ]
 };
